59680eb0924f5898e3c73ee22ee2e1edd65864c8
Elena Dinca <elenadinca25@gmail.com>
2020-04-02 11:29:48 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,7 @@
 # Write your code here
-count = 0
-while count < 8  do
-  puts "I am the #{count} , I love to count!"
-  count += 1
+count = 8
+while count >= 1  do
+  puts count
+  count -= 1
 end
+puts "Hello FlatIron!"
